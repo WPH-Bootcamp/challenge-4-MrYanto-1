8206d2e4f770a05bef87056570e5d261ff32c4b4
Yanto <yanto.santoso1@gmail.com>
2026-05-25 23:01:07 +0700

    main loop of the calculator is done and did some bug fixing at the getNumber

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log('What it can do:');
 console.log("-Find the result you're looking");
 console.log('-Integer or Decimals');
 console.log('-Positive or Negative number');
-console.log('-Even or Odd number');
+console.log('-Even or Odd number \n');
 
 // Available Operators
 
@@ -51,7 +51,7 @@ function getNumber(label) {
     const input = prompt(label);
     const num = Number(input);
 
-    if (num.trim() === '' || isNaN(num)) {
+    if (input.trim() === '' || isNaN(num)) {
       console.log('Invalid Input, please enter a number!');
     } else {
       return num;
@@ -70,4 +70,27 @@ function getOperator() {
       `Invalid operator! Choose from ${Object.keys(operations.join(', '))}    `
     );
   }
+}
+
+// Main Loop
+
+while (true) {
+  const num1 = getNumber('Enter your first number: ');
+  const op = getOperator();
+  const num2 = getNumber('Enter your second number: ');
+
+  const result = operations[op](num1, num2);
+  const IntDec = Number.isInteger(result) ? 'Integer' : 'Decimals';
+  const PosNeg = result < 0 ? 'Negative' : 'Positive';
+  const EvenOdd = result % 2 === 0 ? 'Even' : 'Odd';
+  console.log(`\n Your result of ${num1} ${op} ${num2} is ${result}`);
+  console.log(' =============================');
+  console.log(' About your result:');
+  console.log(`  Integer or Decimals: ${IntDec}`);
+  console.log(`  Positive or Negative: ${PosNeg}`);
+  console.log(`  Even or Odd: ${EvenOdd}`);
+
+  const again = prompt('Want to calculate another number? y/n: ').trim()
+    .toLowerCase;
+  if (again !== 'y') break;
 }
