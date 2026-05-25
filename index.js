408ec27fdf8d79658027453b93357f097944de7e
Yanto <yanto.santoso1@gmail.com>
2026-05-25 22:20:00 +0700

    getNumber function and operations object

--- index.js
+++ index.js
@@ -12,27 +12,49 @@ console.log('-Even or Odd number');
 
 // Available Operators
 
-const add = function(a, b) {
-    return a + b
-}
+const add = function (a, b) {
+  return a + b;
+};
 
-const subtract = function(a, b) {
-    return a - b
-}
+const subtract = function (a, b) {
+  return a - b;
+};
 
-const multiply = function(a, b) {
-    return a * b
-}
+const multiply = function (a, b) {
+  return a * b;
+};
 
-const divide = function(a, b) {
-    return a / b
-}
+const divide = function (a, b) {
+  return a / b;
+};
 
-const modulo = function(a, b) {
-    return a % b
-}
+const modulo = function (a, b) {
+  return a % b;
+};
 
-const power = function(a, b) {
-    return a ** b
-}
+const power = function (a, b) {
+  return a ** b;
+};
 
+// Ask
+const operations = {
+  '+': add,
+  '-': subtract,
+  '*': multiply,
+  '/': divide,
+  '%': modulo,
+  '**': power,
+};
+
+function getNumber(label) {
+  while (true) {
+    const input = prompt(label);
+    const num = Number(input);
+
+    if (num.trim() === '' || isNaN(num)) {
+      console.log('Invalid Input, please enter a number!');
+    } else {
+      return num;
+    }
+  }
+}
