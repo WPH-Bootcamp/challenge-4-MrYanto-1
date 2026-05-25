0b0730ed8db8e103f11c2bf5f28698f39bcd7801
Yanto <yanto.santoso1@gmail.com>
2026-05-25 21:14:56 +0700

    Calculator Introduction

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code here, goodluck!!
+const prompt = require('prompt-sync')({ sigint: true });
+
+console.log('Terminal Calculator');
+console.log('====================');
+console.log('What it can do:');
+console.log("-Find the result you're looking");
+console.log('-Integer or Decimals');
+console.log('-Positive or Negative number');
+console.log('-Even or Odd number');
