85ee2a3109de135cc9d1f2cf9d8eca089c0acd02
Yanto <yanto.santoso1@gmail.com>
2026-05-25 21:24:52 +0700

    feat: Available Operator Function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
+// Introduction
+
 console.log('Terminal Calculator');
 console.log('====================');
 console.log('What it can do:');
@@ -7,3 +9,30 @@ console.log("-Find the result you're looking");
 console.log('-Integer or Decimals');
 console.log('-Positive or Negative number');
 console.log('-Even or Odd number');
+
+// Available Operators
+
+const add = function(a, b) {
+    return a + b
+}
+
+const subtract = function(a, b) {
+    return a - b
+}
+
+const multiply = function(a, b) {
+    return a * b
+}
+
+const divide = function(a, b) {
+    return a / b
+}
+
+const modulo = function(a, b) {
+    return a % b
+}
+
+const power = function(a, b) {
+    return a ** b
+}
+
